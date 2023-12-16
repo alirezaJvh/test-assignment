368ffd41048a0c576719face0762b7b30b28df29
alirezaJavaheri <alireza.jvh98@gmail.com>
2023-12-16 12:55:59 +0330

    ci: confige eslint with prettier

---
+++ .prettierrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+  tabWidth: 2,
+  printWidth: 80,
+  endOfLine: 'auto',
+  arrowParens: 'avoid',
+  semi: true,
+  singleQuote: true,
+  bracketSpacing: true,
+};
--- app/page.tsx
+++ app/page.tsx
@@ -15,7 +15,7 @@ export default function Home() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            By{' '} test
+            By{' '} test test
             <Image
               src="/vercel.svg"
               alt="Vercel Logo"
