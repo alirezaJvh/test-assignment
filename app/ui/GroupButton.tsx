461d6b7cdaa5681dc1aeb468973dab4f422d2949
alirezaJavaheri <alireza.jvh98@gmail.com>
2023-12-16 17:15:15 +0330

    feat: add group-button component

---
+++ app/ui/GroupButton.tsx
@@ -0,0 +1,48 @@
+import { useState, useCallback } from 'react';
+
+interface ToggleButtonProps<T> {
+  buttonList: T[];
+  initialButton?: T;
+  handleClick: (value: T) => void;
+}
+
+const BASE_TOGGLE_BUTTON_CLASSES =
+  'bg-gray-200 flex items-center p-1 rounded-lg text-sm text-gray-600';
+const BUTTON_CLASSES =
+  'grow px-4 py-3 min-w-[130px] rounded-lg transition-colors duration-500';
+const ACTIVE_CLASSES = 'bg-white font-bold shadow-toggle text-black';
+
+export function GroupButton<T>({
+  buttonList,
+  initialButton,
+  handleClick,
+}: ToggleButtonProps<T>) {
+  const [currentButton, setCurrentButton] = useState(initialButton);
+
+  const handleToggle = useCallback(
+    (choosenButton: T) => {
+      buttonList.forEach(button => {
+        if (choosenButton === button) setCurrentButton(button);
+      });
+      handleClick(choosenButton);
+    },
+    [buttonList, handleClick],
+  );
+
+  return (
+    <div className={`${BASE_TOGGLE_BUTTON_CLASSES}`}>
+      {buttonList.map(button => (
+        <button
+          key={`${button}`}
+          type="button"
+          onClick={() => handleToggle(button)}
+          className={`${BUTTON_CLASSES} ${
+            currentButton === button && ACTIVE_CLASSES
+          }`}
+        >
+          {button as string}
+        </button>
+      ))}
+    </div>
+  );
+}
