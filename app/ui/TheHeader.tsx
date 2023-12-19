49a3e20e8a42288118c1cc46948e63d053c3d2fc
alirezaJavaheri <alireza.jvh98@gmail.com>
2023-12-19 09:00:41 +0330

    fix: bug in header

--- app/ui/TheHeader.tsx
+++ app/ui/TheHeader.tsx
@@ -1,15 +1,13 @@
 import React from "react";
-import { useSession } from "next-auth/react";
 
 export function TheHeader() {
-  const { data: session } = useSession();
   return (
     <header className="w-full h-[80px] py-6 flex justify-center border-b border-solid border-gray-300">
       <div className="flex justify-between w-full max-w-[1140px]">
         <div className="absolute w-[calc(100%-48px)] flex justify-center max-w-[1172px] z-[-1]">
-          <div>Logo</div>
+          <div>salam</div>
         </div>
-        <div className="flex items-center">{session?.user?.name}</div>
+        <div className="flex items-center">you are login</div>
       </div>
     </header>
   );
